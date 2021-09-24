ea1cb0d1c01a3b796b5b576be26b641ce9ff6fe2
prabhug1987 <prabhug1987@gmail.com>
2021-09-24 22:43:11 +0530

    first commit

---
+++ src/test/java/com/ganapathylingam/prabhu/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganapathylingam.prabhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
